9cb8878de96bc0db4bc0d13939525dab51bdd8a5
sdrogomix <simoneciccarelli2718@gmail.com>
2024-10-16 21:20:52 +0200

    Create README.c

---
+++ README.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void hi() {
+printf("im really interested in C language\n");
+printf("also i want to understand how to optimize code better\n");
+printf("also someday i wanna be able to use scanf flawlessly\n");
+printf("im majoring in math\n");
+exit(0);
+}
+
+int main() {
+hi;
+printf("i wonder if anyones reading this");
+return 0;
+}
